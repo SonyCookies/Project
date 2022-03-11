fd3f30fe6daa31220edee69d89622f1160deaa66
SonyCookies <toxicookie.v1@gmail.com>
2022-03-11 15:55:24 +0800

    MAIN

---
+++ app/src/main/java/com/example/project/ui/screens/HomePage.kt
@@ -0,0 +1,17 @@
+package com.example.project.ui.screens
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StartScreen(
+
+) {
+    Scaffold(
+        topBar = {
+            HomeTopBar()
+        },
+        content = {
+        },
+    )
+}
